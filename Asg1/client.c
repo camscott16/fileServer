3dcac4908656a778c93679f9da21f5bef5a4d0d8
Cameron Scott <casctt@clemson.edu>
2024-10-21 18:04:04 -0400

    init: curr prog

---
+++ Asg1/client.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include "Practical.h"
+
+#define BUFSIZE 512
+
+void PrintMenu();
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) {  // Test for correct number of arguments
+		DieWithUserMessage("Parameter(s)", "<Server IP> <Server Port>");
+	}
+
+	char *servIP = argv[1];     // Server IP address (dotted quad)
+	in_port_t servPort = atoi(argv[2]);  // Server port
+
+	// Create a reliable, stream socket using TCP
+	int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock < 0) {
+		DieWithSystemMessage("socket() failed");
+	}
+
+	// Construct the server address structure
+	struct sockaddr_in servAddr;
+	memset(&servAddr, 0, sizeof(servAddr));  // Zero out structure
+	servAddr.sin_family = AF_INET;           // IPv4 address family
+	int rtnVal = inet_pton(AF_INET, servIP, &servAddr.sin_addr.s_addr);
+	if (rtnVal == 0) {
+		DieWithUserMessage("inet_pton() failed", "invalid address string");
+	} else if (rtnVal < 0) {
+		DieWithSystemMessage("inet_pton() failed");
+	}
+	servAddr.sin_port = htons(servPort);  // Server port
+
+	// Establish the connection to the server
+	if (connect(sock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
+		DieWithSystemMessage("connect() failed");
+	}
+
+	printf("Connected to server %s\n", servIP);
+
+	char buffer[BUFSIZE];
+	char accumulatedBuffer[BUFSIZE * 4];  // Buffer to accumulate data until a full line is received
+	int accumulatedLen = 0;
+	int choice;
+	int command;
+
+	PrintMenu();
+	while (scanf("%d", &choice) != EOF) {
+		getchar();  // Consume newline
+		switch (choice) {
+			case 1:  // Request file listing
+				command = 1;
+				send(sock, &command, sizeof(command), 0);
+				printf(">> Listing received from server:\n");
+				printf("filename\tsize\tinode\n");
+				accumulatedLen = 0;
+				while ((rtnVal = recv(sock, buffer, BUFSIZE - 1, 0)) > 0) {
+					buffer[rtnVal] = '\0';
+					strncat(accumulatedBuffer + accumulatedLen, buffer, BUFSIZE * 4 - accumulatedLen - 1);
+					accumulatedLen += rtnVal;
+
+					char *lineEnd;
+					while ((lineEnd = strchr(accumulatedBuffer, '\n')) != NULL) {
+						*lineEnd = '\0';
+						char filename[256];
+						long size, inode;
+						int parsed = sscanf(accumulatedBuffer, "%255s %ld bytes %ld", filename, &size, &inode);
+						if (parsed == 3) {
+							printf("%s\t%ld bytes\t%ld\n", filename, size, inode);
+						} else {
+							printf("Error parsing file information: %s\n", accumulatedBuffer);
+						}
+
+						// Move remaining data to the start of accumulatedBuffer
+						memmove(accumulatedBuffer, lineEnd + 1, accumulatedLen - (lineEnd - accumulatedBuffer + 1));
+						accumulatedLen -= (lineEnd - accumulatedBuffer + 1);
+						accumulatedBuffer[accumulatedLen] = '\0';
+					}
+				}
+				break;
+			case 2:  // Download file
+
+				printf(">> Enter filename you would like to download:\n>> ");
+				fgets(buffer, BUFSIZE, stdin);
+				buffer[strcspn(buffer, "\n")] = 0;  // Remove newline
+				command = 2;
+				send(sock, &command, sizeof(command), 0);
+				send(sock, buffer, strlen(buffer), 0);
+				FILE *downloadFile = fopen(buffer, "wb");
+				if (downloadFile == NULL) {
+					perror("Cannot create file");
+					break;
+				}
+				while ((rtnVal = recv(sock, buffer, BUFSIZE, 0)) > 0) {
+					fwrite(buffer, 1, rtnVal, downloadFile);
+				}
+				fclose(downloadFile); 
+				printf("Downloaded %s", downloadFile);
+				break;
+
+			case 3:  // Upload file
+
+				printf(">> Enter filename to upload:\n>> ");
+				fgets(buffer, BUFSIZE, stdin);
+				buffer[strcspn(buffer, "\n")] = 0;  // Remove newline
+				command = 3;
+				send(sock, &command, sizeof(command), 0);
+				send(sock, buffer, strlen(buffer), 0);
+
+				struct stat fileStat;
+				if (stat(buffer, &fileStat) < 0) {
+					perror("Cannot retrieve file information");
+					break;
+				}
+				FILE *uploadFile = fopen(buffer, "rb");
+				if (uploadFile == NULL) {
+					perror("Cannot open file");
+					break;
+				}
+				while ((rtnVal = fread(buffer, 1, BUFSIZE, uploadFile)) > 0) {
+					send(sock, buffer, rtnVal, 0);
+				}
+				fclose(uploadFile);
+				printf("Uploaded: %s", uploadFile);
+				break;
+			case 4:  // Exit
+				printf("\n>> Goodbye!!!\n");
+				command = 4;
+				send(sock, &command, sizeof(command), 0);
+				close(sock);
+				exit(0);
+				break;
+			default:
+				printf("Invalid choice. Please try again.\n");
+		}
+		PrintMenu();
+	}
+
+	close(sock);
+	return 0;
+}
+
+void PrintMenu() {
+	printf("\nSelect an action from the menu:\n");
+	printf("1. Request file listing\n");
+	printf("2. Download file\n");
+	printf("3. Upload file\n");
+	printf("4. Exit\n");
+	printf(">> ");
+}
+
---
+++ Asg1/server.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include "Practical.h"
+
+#define MAXPENDING 5  // Maximum number of pending connections
+
+void HandleClient(int clntSock);
+
+int main(int argc, char *argv[]) {
+
+	if (argc != 2) {  // Test for correct number of arguments
+		DieWithUserMessage("Parameter(s)", "<Server Port>");
+	}
+
+	in_port_t servPort = atoi(argv[1]);  // Local port
+
+	// Create socket for incoming connections
+	int servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (servSock < 0) {
+		DieWithSystemMessage("socket() failed");
+	}
+
+	// Construct local address structure
+	struct sockaddr_in servAddr;
+	memset(&servAddr, 0, sizeof(servAddr));  // Zero out structure
+	servAddr.sin_family = AF_INET;           // IPv4 address family
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);  // Any incoming interface
+	servAddr.sin_port = htons(servPort);     // Local port
+
+	// Bind to the local address
+	if (bind(servSock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
+		DieWithSystemMessage("bind() failed");
+	}
+
+	// Listen for incoming connections
+	if (listen(servSock, MAXPENDING) < 0) {
+		DieWithSystemMessage("listen() failed");
+	}
+
+	printf("Welcome to Simple File Server\n");
+
+	for (;;) {  // Run forever
+		struct sockaddr_in clntAddr;
+		socklen_t clntAddrLen = sizeof(clntAddr);
+
+		// Wait for a client to connect
+		int clntSock = accept(servSock, (struct sockaddr *)&clntAddr, &clntAddrLen);
+		if (clntSock < 0) {
+			DieWithSystemMessage("accept() failed");
+		}
+
+		// clntSock is connected to a client
+		char clntName[INET_ADDRSTRLEN];
+		if (inet_ntop(AF_INET, &clntAddr.sin_addr.s_addr, clntName, sizeof(clntName)) != NULL) {
+			printf("Connected to IP: %s\n", clntName);
+		} else {
+			puts("Unable to get client address");
+		}
+
+		HandleClient(clntSock);
+		printf("Connection with %s is terminated\n", clntName);
+	}
+}
+
+void HandleClient(int clntSock) {
+
+	int command;
+	char buffer[BUFSIZE];
+	ssize_t numBytesRcvd;
+	int keepRunning = 1;
+
+	while (keepRunning) {
+		numBytesRcvd = recv(clntSock, &command, sizeof(command), 0);
+		if (numBytesRcvd < 0) {
+			DieWithSystemMessage("recv() failed");
+		} else if (numBytesRcvd == 0) {
+			// Client disconnected
+			break;
+		}
+
+		switch (command) {
+			case 1:  // LIST
+
+				printf("-- client requested file listing\n");
+				// Send file listing
+				DIR *dir = opendir(".");
+				if (dir == NULL) {
+					DieWithSystemMessage("opendir() failed");
+				}
+
+				struct dirent *entry;
+				while ((entry = readdir(dir)) != NULL) {
+					if (entry->d_type == DT_REG) {  // Only regular files
+						struct stat fileStat;
+						if (stat(entry->d_name, &fileStat) < 0) {
+							continue;
+						}
+						snprintf(buffer, BUFSIZE, "%s %ld bytes %ld\n", entry->d_name, (long)fileStat.st_size, (long)fileStat.st_ino);
+						send(clntSock, buffer, strlen(buffer), 0);
+					}
+				}
+				closedir(dir);
+				break;
+
+			case 2:  // DOWNLOAD
+
+				numBytesRcvd = recv(clntSock, buffer, BUFSIZE - 1, 0);
+				if (numBytesRcvd <= 0) {
+					break;
+				}
+				buffer[numBytesRcvd] = '\0';
+				printf("-- client requested to download %s\n", buffer);
+
+				FILE *file = fopen(buffer, "rb");
+				if (file == NULL) {
+					snprintf(buffer, BUFSIZE, "ERROR: File not found\n");
+					send(clntSock, buffer, strlen(buffer), 0);
+				} else {
+					while ((numBytesRcvd = fread(buffer, 1, BUFSIZE, file)) > 0) {
+						send(clntSock, buffer, numBytesRcvd, 0);
+					}
+					fclose(file);
+					printf("Sent %s to the client\n", buffer);
+				}
+				break;
+
+			case 3:  // UPLOAD
+
+				numBytesRcvd = recv(clntSock, buffer, BUFSIZE - 1, 0);
+				if (numBytesRcvd <= 0) {
+					break;
+				}
+				buffer[numBytesRcvd] = '\0';
+				printf("-- client requested to upload: %s\n", buffer);
+
+				FILE *fileUpload = fopen(buffer, "wb");
+				if (fileUpload == NULL) {
+					snprintf(buffer, BUFSIZE, "ERROR: Cannot create file\n");
+					send(clntSock, buffer, strlen(buffer), 0);
+				} else {
+					while ((numBytesRcvd = recv(clntSock, buffer, BUFSIZE, 0)) > 0) {
+						fwrite(buffer, 1, numBytesRcvd, fileUpload);
+					}
+					fclose(fileUpload);
+					printf("Received %s from the client\n", buffer);
+				}
+
+				break;
+
+			case 4:  // EXIT
+
+				printf("-- client requested to exit\n");
+				keepRunning = 0;
+				break;
+
+			default:
+				snprintf(buffer, BUFSIZE, "ERROR: Unknown command\n");
+				send(clntSock, buffer, strlen(buffer), 0);
+				break;
+		}
+	}
+
+	close(clntSock);  // Close client socket
+}
+
